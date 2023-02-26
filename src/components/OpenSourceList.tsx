6e7127101708f0b81d6729071aa23985d9dc5709
Andre Alves <andremralves@gmail.com>
2023-02-26 02:04:32 -0300

    add first open source project

--- src/components/OpenSourceList.tsx
+++ src/components/OpenSourceList.tsx
@@ -7,10 +7,23 @@ const OpenSourceList = (props: OpenSourceListProps) => {
   return (
     <section>
       <SectionTitle title="Open Source" />
-      <div>
-        <h2 className="text-xl">Teammates</h2>
-        <span>****</span>
-        <p>Description</p>
+      <div className="bg-neutral-900 rounded p-2 mt-8 border-orange-300 border-l-2">
+        <div className="ml-2">
+          <div className="flex items-center justify-between">
+            <h2 className="text-xl">Teammates</h2>
+            <span className="h-fit ml-2 px-2 bg-neutral-600 rounded">
+              ⭐ 1.4k stars
+            </span>
+          </div>
+          <h3 className="mt-3">Description</h3>
+          <p className="mt-3 text-neutral-400">
+            "TEAMMATES is a free online tool for managing peer evaluations and
+            other feedback paths of your students. It is provided as a
+            cloud-based service for educators/students and is currently used by
+            hundreds of universities across the world."
+          </p>
+          <button className="mt-3">My contributions</button>
+        </div>
       </div>
     </section>
   )
