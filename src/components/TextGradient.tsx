4cc537e8986703d697a5cfab099c780f70c22436
Andre Alves <andremralves@gmail.com>
2023-02-26 00:37:55 -0300

    add text gradient

---
+++ src/components/TextGradient.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+type TextGradientProps = {
+  text: string
+  from?: string
+  via?: string
+  to?: string
+}
+
+export default function TextGradient(props: TextGradientProps) {
+  const from = props.from || 'from-orange-700'
+  const via = props.via || 'via-blue-500'
+  const to = props.to || 'to-green-400'
+
+  return (
+    <span
+      className={`bg-gradient-to-r ${from} ${via} ${to} text-transparent bg-clip-text animate-gradient`}
+    >
+      {props.text}
+    </span>
+  )
+}
