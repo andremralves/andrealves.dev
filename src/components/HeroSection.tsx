3078c75cae5ed791f3e6401190fad078a4a21a68
Andre Alves <andremralves@gmail.com>
2023-02-26 08:54:28 -0300

    add social and remove posts

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import Social from './Social'
+import TextGradient from './TextGradient'
 
 type Props = {}
 
 const HeroSection = (props: Props) => {
-  return <div>HeroSection</div>
+  return (
+    <section className="py-10">
+      <div className="flex flex-col">
+        <div className="max-w-fit mb-5 text-3xl">
+          <span>Hi there 👋, I'm</span>
+          <h1 className="pt-2 text-7xl font-semibold">
+            <TextGradient text="André Alves" />
+          </h1>
+        </div>
+        <p className="text-neutral-400">
+          Software engineering student and full stack web developer that likes
+          to contribute to open source projects.
+        </p>
+      </div>
+      <Social />
+    </section>
+  )
 }
 
 export default HeroSection
--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa'
 
 type Props = {}
 
 const Social = (props: Props) => {
-  return <div></div>
+  return (
+    <div className="mt-8">
+      <ul className="flex gap-5">
+        <li>
+          <a
+            target="_blank"
+            rel="nofollow noopener noreferrer"
+            href="https://github.com/andremralves"
+          >
+            <FaGithub size={30} />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="nofollow noopener noreferrer"
+            href="https://www.linkedin.com/in/andremralves/"
+          >
+            <FaLinkedin size={30} className="text-blue-400" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="nofollow noopener noreferrer"
+            href="mailto:andremralves@gmail.com"
+          >
+            <FaEnvelope size={30} className="text-orange-400" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default Social
