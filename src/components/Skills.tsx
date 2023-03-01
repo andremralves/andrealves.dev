ded6ddfcbb0b166e4c393a70c1217a3d32f83fdf
Andre Alves <andremralves@gmail.com>
2023-03-01 15:44:29 -0300

    add skills section

---
+++ src/components/Skills.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import SectionTitle from './SectionTitle'
+import Skill from './Skill'
+import Tag from './Tag'
+
+type Props = {}
+
+const Skills = (props: Props) => {
+  return (
+    <section className="mt-10">
+      <SectionTitle title="Skills" />
+      <ul className="mt-8 flex flex-col gap-5 w-fit max-h-40 md:max-h-36 flex-wrap">
+        <Skill name="Typescript" />
+        <Skill name="Java" />
+        <Skill name="ReactJs" />
+        <Skill name="NodeJs" />
+        <Skill name="C/C++" />
+        <Skill name="Mysql" />
+        <Skill name="PostgreSql" />
+        <Skill name="Git" />
+        <Skill name="Linux" />
+      </ul>
+    </section>
+  )
+}
+
+export default Skills
