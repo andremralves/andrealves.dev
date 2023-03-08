c5ad16c8a7585e1c37d9c4c557a81c1e051468cf
Andre Alves <andremralves@gmail.com>
2023-03-08 17:06:01 -0300

    update url

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://andrealves.dev',
+  site: 'https://www.andrealves.dev',
   integrations: [tailwind(), react(), sitemap()],
   markdown: {
     shikiConfig: {
