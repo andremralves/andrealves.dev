37b328105b0461aabeea2b2be2fe0969db37f7de
Andre Alves <andremralves@gmail.com>
2023-03-15 12:04:35 -0300

    fex react warnings

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -16,7 +16,10 @@ const Posts = (props: PostsProps) => {
       <SectionTitle title={props.title} />
       <ul className="mt-4">
         {props.posts.map((post, index) => (
-          <li className="pl-2 py-4 border-neutral-500 border-b hover:bg-neutral-800 rounded">
+          <li
+            key={post.frontmatter.title}
+            className="pl-2 py-4 border-neutral-500 border-b hover:bg-neutral-800 rounded"
+          >
             <a className="flex" href={post.url + '/'}>
               <span className="mr-3 text-neutral-500">{index + 1}</span>
               <div className="flex flex-col gap-2 justify-between w-full">
@@ -24,7 +27,7 @@ const Posts = (props: PostsProps) => {
                   <h1 className="text-xl">{post.frontmatter.title}</h1>
                   <div className="flex flex-wrap gap-2 my-4">
                     {post.frontmatter.tags.map((tag) => (
-                      <Tag name={tag} />
+                      <Tag key={tag} name={tag} />
                     ))}
                   </div>
                   <p className="text-neutral-400">
