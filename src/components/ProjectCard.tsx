f96980e5839274181a3e899adbb742782981b50c
Andre Alves <andremralves@gmail.com>
2023-03-23 02:35:11 -0300

    add images to projects and +1 project

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Tag from './Tag'
 import SimpleDashedButton from './SimpleDashedButton'
 
-type ProjectCardProps = {
+export type ProjectCardProps = {
   projectName: string
   borderColor?: string
   techs: string[]
   description: string
   sourceLink?: string
   liveLink?: string
+  projectImageSrc?: string
 }
 
 const ProjectCard = (props: ProjectCardProps) => {
@@ -16,35 +17,54 @@ const ProjectCard = (props: ProjectCardProps) => {
     <div
       className={`bg-neutral-900 ${props.borderColor} rounded p-2 mt-8 border-l-2 drop-shadow-md`}
     >
-      <div className="ml-2">
-        <h2 className="text-2xl">{props.projectName}</h2>
-        <h3 className="mt-3">Tech</h3>
-        <div className="flex mt-1 gap-2">
-          {props.techs.map((tech) => (
-            <Tag key={tech} name={tech} />
-          ))}
+      <div className="ml-2 flex flex-col md:flex-row md:items-center">
+        <div className="md:w-3/5 md:pr-2">
+          <h2 className="text-2xl">{props.projectName}</h2>
+          <div className="md:hidden block mt-3">
+            <img
+              className="rounded border border-neutral-500"
+              src={props.projectImageSrc}
+              alt=""
+            />
+          </div>
+          <h3 className="mt-3">Tech</h3>
+          <div className="flex mt-1 gap-2">
+            {props.techs.map((tech) => (
+              <Tag key={tech} name={tech} />
+            ))}
+          </div>
+          <h3 className="mt-3">Description</h3>
+          <p className="mt-3 text-neutral-400">{props.description}</p>
+          <div className="flex gap-5 mt-3">
+            {props.sourceLink && (
+              <a
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+                href={props.sourceLink}
+              >
+                <SimpleDashedButton text="Source Code" />
+              </a>
+            )}
+            {props.liveLink && (
+              <a
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+                href={props.liveLink}
+              >
+                <SimpleDashedButton
+                  text="Live project"
+                  color="text-orange-300"
+                />
+              </a>
+            )}
+          </div>
         </div>
-        <h3 className="mt-3">Description</h3>
-        <p className="mt-3 text-neutral-400">{props.description}</p>
-        <div className="flex gap-5 mt-3">
-          {props.sourceLink && (
-            <a
-              target="_blank"
-              rel="nofollow noopener noreferrer"
-              href={props.sourceLink}
-            >
-              <SimpleDashedButton text="Source Code" />
-            </a>
-          )}
-          {props.liveLink && (
-            <a
-              target="_blank"
-              rel="nofollow noopener noreferrer"
-              href={props.liveLink}
-            >
-              <SimpleDashedButton text="Live project" color="text-orange-300" />
-            </a>
-          )}
+        <div className="hidden md:block md:w-2/5">
+          <img
+            className="rounded border border-neutral-500"
+            src={props.projectImageSrc}
+            alt=""
+          />
         </div>
       </div>
     </div>
--- src/components/ProjectsList.tsx
+++ src/components/ProjectsList.tsx
@@ -1,32 +1,45 @@
 import React from 'react'
-import ProjectCard from './ProjectCard'
+import ProjectCard, { ProjectCardProps } from './ProjectCard'
 import SectionTitle from './SectionTitle'
 
-const projects = [
+const projects: ProjectCardProps[] = [
   {
-    name: 'This Website',
+    projectName: 'Tile Installation Calculator',
+    techs: ['typescript', 'reactjs', 'tailwindcss'],
+    borderColor: 'border-neutral-100',
+    description:
+      "An app built for a local business that estimates the cost their tile installation service given the customer's area and tile of choice.",
+    sourceLink: '',
+    liveLink: 'https://master--effortless-fenglisu-62b59d.netlify.app/',
+    projectImageSrc: '/images/tile-calculator.jpg',
+  },
+  {
+    projectName: 'This Website',
     techs: ['typescript', 'reactjs', 'astrojs'],
     borderColor: 'border-neutral-100',
     description: 'My personal portfolio website and blog.',
     sourceLink: 'https://github.com/andremralves/new_website',
     liveLink: '',
+    projectImageSrc: '/images/website-project.jpg',
   },
   {
-    name: 'Django CRM',
+    projectName: 'Django CRM',
     techs: ['python', 'django', 'html', 'css', 'bootstrap'],
     borderColor: 'border-orange-300',
     description: 'A CRM system to manage patients and appointments.',
     sourceLink: 'https://github.com/andremralves/django-CRM',
     liveLink: '',
+    projectImageSrc: '/images/django-project.jpg',
   },
   {
-    name: 'Todo List',
+    projectName: 'Todo List',
     techs: ['typescript', 'reactjs', 'tailwindcss'],
     borderColor: 'border-blue-300',
     description:
       'A Todo List app built with Typescript, TailwindCss, React and using Local Storage to store your tasks.',
     sourceLink: 'https://github.com/andremralves/react-tailwind-todolist',
     liveLink: 'https://serene-salmiakki-a825e6.netlify.app/',
+    projectImageSrc: '/images/todolist-project.jpg',
   },
 ]
 
@@ -38,13 +51,14 @@ const ProjectsList = (props: ProjectListProps) => {
       <SectionTitle title="Projects" />
       {projects.map((project) => (
         <ProjectCard
-          key={project.name}
-          projectName={project.name}
+          key={project.projectName}
+          projectName={project.projectName}
           techs={project.techs}
           description={project.description}
           borderColor={project.borderColor}
           sourceLink={project.sourceLink}
           liveLink={project.liveLink}
+          projectImageSrc={project.projectImageSrc}
         />
       ))}
     </>
