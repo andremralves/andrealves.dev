3a713f3b4a26dbf2eefcc723a58c94099f9b4617
Andre Alves <andremralves@gmail.com>
2023-06-15 09:08:38 -0300

    Remove unused tailwind animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,26 +14,13 @@ module.exports = {
           '50%': { backgroundPosition: '100% 50%' },
           '100%': { backgroundPosition: '0% 50%' },
         },
-        wave: {
-          '0%': { transform: 'rotate(0.0deg)' },
-          '10%': { transform: 'rotate(14deg)' },
-          '20%': { transform: 'rotate(-8deg)' },
-          '30%': { transform: 'rotate(14deg)' },
-          '40%': { transform: 'rotate(-4deg)' },
-          '50%': { transform: 'rotate(10.0deg)' },
-          '60%': { transform: 'rotate(0.0deg)' },
-          '100%': { transform: 'rotate(0.0deg)' },
-        },
       },
-
       backgroundSize: {
         '300%': '300%',
       },
-
       animation: {
         gradient: 'animatedgradient 6s ease infinite alternate',
       },
-      'waving-hand': 'wave 2s linear infinite',
     },
   },
   plugins: [require('@tailwindcss/typography')],
