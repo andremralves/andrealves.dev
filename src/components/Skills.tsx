10643a43703c135799b09418fbd4f77a5ff45df2
Andre Alves <andremralves@gmail.com>
2023-06-15 09:59:29 -0300

    Add padding to skills list

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -9,7 +9,7 @@ const Skills = (props: Props) => {
   return (
     <section className="mt-10">
       <SectionTitle title="Skills" />
-      <ul className="mt-8 flex flex-col gap-5 w-fit max-h-40 md:max-h-36 flex-wrap">
+      <ul className="pl-4 mt-8 flex flex-col gap-5 w-fit max-h-40 md:max-h-36 flex-wrap">
         <Skill name="Typescript" />
         <Skill name="Java" />
         <Skill name="ReactJs" />
