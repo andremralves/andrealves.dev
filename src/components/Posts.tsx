17b8757242e13a550853c0b67b35cd9212097e23
Andre Alves <andremralves@gmail.com>
2023-09-09 11:24:28 -0300

    Migrate to content collections and add prettier

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -11,28 +11,29 @@ const Posts = (props: PostsProps) => {
   function dateFormatter(date: string) {
     return new Date(Date.parse(date)).toLocaleDateString()
   }
+  console.log(props.posts[0].slug)
   return (
     <section className="mt-10">
       <SectionTitle title={props.title} />
       <ul className="mt-4">
-        {props.posts.map((post, index) => (
+        {props.posts.map((post) => (
           <li
-            key={post.frontmatter.title}
+            key={post.data.title}
             className="p-4 border-zinc-500 border-b dark:hover:bg-zinc-800 hover:bg-zinc-200 rounded"
           >
-            <a className="flex" href={post.url + '/'}>
+            <a className="flex" href={'/blog/' + post.slug + '/'}>
               <div className="flex flex-col gap-2 justify-between w-full">
                 <div>
-                  <h1 className="text-xl">{post.frontmatter.title}</h1>
+                  <h1 className="text-xl">{post.data.title}</h1>
                   <div className="flex flex-wrap gap-2 my-4">
-                    {post.frontmatter.tags.map((tag) => (
+                    {post.data.tags.map((tag) => (
                       <Tag key={tag} name={tag} />
                     ))}
                   </div>
-                  <p>{post.frontmatter.description}</p>
+                  <p>{post.data.description}</p>
                 </div>
 
-                <div>{dateFormatter(post.frontmatter.pubDate)}</div>
+                <div>{dateFormatter(post.data.pubDate)}</div>
               </div>
             </a>
           </li>
